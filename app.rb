5760e1ba32d578744545638d783f012257f0a5d0
Steve Albonico <salbonico@gmail.com>
2019-02-05 01:43:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS! steve is cool!"
+    "Started my server using Shotgun!"
   end
 
 end
